3fafd494f3a2412c2ce9ab6366406f9e370b52fc
Mark <f18846188605@gmail.com>
2019-04-18 22:17:12 +0800

    finish getting first set

--- lab2/src/main/java/Main.java
+++ lab2/src/main/java/Main.java
@@ -2,18 +2,70 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
+    public static Set<String> terminators = new HashSet<>(), non_terminators = new HashSet<>();
+    public static List<Formula> grammars = new ArrayList<>();
+    public static Map<String, Set<String>> firstSets = new HashMap<>();
+    public static String nullString = "ε";
+
     public static void main(String[] args) throws Exception {
-        String grammarInputFile = "grammar.txt";
-        List<String> terminators = new ArrayList<>(), non_terminators = new ArrayList<>();
-        List<Formula> grammars = new ArrayList<>();
-        readGrammar(terminators, non_terminators, grammars, grammarInputFile);
+        String grammarInputFile = "grammarTest.txt";
+        readGrammar(grammarInputFile);
+        for (String term : non_terminators) {
+            firstSets.put(term, getFirstSet(term));
+        }
+        System.out.println(firstSets.toString());
     }
 
-    public static void readGrammar(List<String> terminators, List<String> non_terminators, List<Formula> grammars,
-            String filePath) throws IOException {
+    public static Set<String> getFirstSet(String prefix) {
+        Set<String> firstSet = new HashSet<>();
+        for (Formula formula : grammars) {
+            if (formula.getPrefix().equals(prefix)) {
+                List<String> symbols = formula.getSymbols();
+                int i = 1;
+                if (i >= symbols.size() + 1)
+                    firstSet.add(nullString);
+                while (i <= symbols.size()) {
+                    String tempString = symbols.get(i - 1);
+                    if (terminators.contains(tempString)) {
+                        firstSet.add(tempString);
+                        break;
+                    }
+                    if (non_terminators.contains(tempString)) {
+                        if (!prefix.equals(tempString)) {
+                            Set<String> tempSet;
+                            if (firstSets.containsKey(tempString)) {
+                                tempSet = firstSets.get(tempString);
+                            } else {
+                                tempSet = getFirstSet(tempString);
+                            }
+                            for (String str : tempSet) {
+                                if (!str.equals(nullString)) {
+                                    firstSet.add(str);
+                                }
+                            }
+                            if (!tempSet.contains(nullString)) {
+                                break;
+                            }
+                        }
+
+                    }
+                    i += 1;
+                }
+                if (i == symbols.size() + 1)
+                    firstSet.add(nullString);
+            }
+        }
+        return firstSet;
+    }
+
+    public static void readGrammar(String filePath) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
         String line;
         boolean terminator = false, non_terminator = false, grammar = false;
